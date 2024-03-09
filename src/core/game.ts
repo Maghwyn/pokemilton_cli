60335629ecc8cdb8dcd72d70c8def9f1606d0df5
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-09 21:00:58 +0100

    feat(part4-1): Setup the required tasks

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,8 +1,9 @@
-import readline from 'readline';
-import PokemiltonMaster from '@/class/PokemiltonMaster'; // Replace 'your_classes_filename' with the actual filename
+import readline from 'node:readline/promises';
+import fs from 'node:fs/promises';
+
+import PokemiltonMaster from '@/class/PokemiltonMaster';
 import Pokemilton from '@/class/Pokemilton';
 import PokemiltonWorld from '@/class/PokemiltonWorld';
-import fs from 'fs';
 
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -15,6 +16,47 @@ function askForName() {}
 
 function proposeFirstPokemilton(rl) {}
 
-function startGame() {}
+function displayPokemiltonList(list: string) {
+	const tabHeader = 'index | name | level | exp | hp | maxHp | isDown';
+	return `${tabHeader}\n${list}`;
+}
 
-startGame();
+async function getMyFirstPokemilton(name: string) {
+	const pokemiltons = [new Pokemilton(), new Pokemilton(), new Pokemilton()];
+
+	const choices = [
+		`1. | ${pokemiltons[0].toString()}`,
+		`2. | ${pokemiltons[1].toString()}`,
+		`3. | ${pokemiltons[2].toString()}`,
+	];
+
+	const answer = await rl.question(
+		`Which pokemilton will you start with ${name} ?\n\n` +
+			`${displayPokemiltonList(choices.join('\n'))}\n`,
+	);
+	const indexChosen = parseInt(answer);
+
+	if (isNaN(indexChosen)) {
+		console.log("You didn't choose a pokemilton");
+		return getMyFirstPokemilton(name); // Ask the question again by calling the function itself (looping)
+	}
+
+	if (indexChosen > pokemiltons.length + 1 || indexChosen < 0) {
+		console.log(`There's no pokemilton for the ${indexChosen}th choice`);
+		return getMyFirstPokemilton(name); // Ask the question again by calling the function itself (looping)
+	}
+
+	return pokemiltons[indexChosen];
+}
+
+async function setupGame() {
+	const world = new PokemiltonWorld();
+	const name = await rl.question('What is your name ?\n');
+	const master = new PokemiltonMaster(name);
+	const pokemilton = await getMyFirstPokemilton(name);
+	master.catchPokemilton(pokemilton);
+
+	console.log(pokemilton.toString());
+}
+
+setupGame();
